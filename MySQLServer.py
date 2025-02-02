32005125d5eaa5e8c0e82b83f9e8e1a774e88967
Karim Tantawy <kareemtantawy01@gmail.com>
2025-02-02 22:28:43 +0200

    Python script to deal with MySQL server

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 try:
     mydb = mysql.connector.connect(host="localhost", user="karim", passwd="root")
-except Exception as err:
+except mysql.connector.Error as err:
     raise err
 else:
     print(f"Database 'alx_book_store' created successfully")
